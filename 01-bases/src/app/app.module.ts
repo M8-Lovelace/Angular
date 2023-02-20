61c6232a57a437c872a9625670a1c66d4956921d
Laura Susano <lauritasusano.10@gmail.com>
2023-02-20 18:34:12 -0500

    feat(project): introduction to angular

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Importancion de componentes
 import { AppComponent } from './app.component';
 
+// Importación de un módulo de Angular
+import { HeroesModule } from './heroes/heroes.module';
+
+// Importación del módulo de contador
+import { ContadorModule } from './contador/contador.module';
+
+// Decorador
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  // Componentes
+  declarations: [AppComponent],
+  // Módulos
+  imports: [BrowserModule, HeroesModule, ContadorModule],
+  // Servicios
   providers: [],
-  bootstrap: [AppComponent]
+  // Componente principal
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
