0a6ef2a559e1a7e62df454486512707b06e9a569
Laura Susano <lauritasusano.10@gmail.com>
2023-03-07 18:38:25 -0500

    feat(project): giphy api implementation and design

--- 03-gifsApp/README.md
+++ 03-gifsApp/README.md
@@ -1,6 +1,6 @@
-# GifsApp
+# GiftApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
 ## Development server
 
--- 03-gifsApp/src/app/app.module.ts
+++ 03-gifsApp/src/app/app.module.ts
@@ -1,16 +1,25 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+// Componentes
 import { AppComponent } from './app.component';
 
+// Módulos
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  // Importación de los componentes
+  declarations: [AppComponent],
+  // Exportación de los componentes
+  exports: [],
+  // Importación de módulos
+  imports: [BrowserModule, HttpClientModule, SharedModule, GifsModule],
+  // Importación de servicios
   providers: [],
-  bootstrap: [AppComponent]
+  // Carga del componente principal
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
